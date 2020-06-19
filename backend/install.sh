5db3360a1ea853055dbba27805f5e92014938de2
icyftl <savap0@yandex.ru>
2020-06-19 06:11:39 +0300

    Travis CI Golang fixes #6

--- backend/install.sh
+++ backend/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-go get https://github.com/jackc/chunkreader/
-go get https://github.com/jackc/pgconn/
-go get https://github.com/jackc/pgio/
-go get https://github.com/jackc/pgpassfile
-go get https://github.com/jackc/pgservicefile/
-go get https://github.com/jackc/puddle/
-go get https://github.com/gorilla/mux/
+go get github.com/jackc/chunkreader/
+go get github.com/jackc/pgconn/
+go get github.com/jackc/pgio/
+go get github.com/jackc/pgpassfile
+go get github.com/jackc/pgservicefile/
+go get github.com/jackc/puddle/
+go get github.com/gorilla/mux/
 
 go build -o ../frontend/dist/backend_compiled -i .
